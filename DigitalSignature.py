98a036c16c7c9063b4913a0daf4d8a026263af95
tomoko <tom.shintani@gmail.com>
2024-02-22 09:32:21 +0900

    Update DigitalSignature.py

--- DigitalSignature.py
+++ DigitalSignature.py
@@ -1,6 +1,7 @@
 from ecdsa import SigningKey
 from ecdsa import SECP256k1
 import time
+import hashlib
 
 
 def ask_confirmation():
@@ -21,7 +22,8 @@ class User:
     def send_data(self, data, receiver):
         data_bytes = bytes(data, encoding= "utf-8")
         self.mail_box[receiver.name] = History(self.name, data, data_bytes, receiver)
-        signatured_data = self.secret_key.sign(data_bytes)
+        hash_data = hashlib.sha256(data_bytes).digest() #これちゃんとdigestしないとbytesで返ってこないので注意
+        signatured_data = self.secret_key.sign(hash_data)
         receiver.receive_data(self, signatured_data)
 
         
@@ -35,15 +37,22 @@ class User:
     def verify_data(self, sender, received_signed_data):
         data = sender.mail_box[self.name].open_data()
         true_data_bytes = sender.mail_box[self.name].data_bytes
-        print(sender.public_key.verify(received_signed_data, true_data_bytes))
+        hash_true_data = hashlib.sha256(true_data_bytes).digest()
+        print(sender.public_key.verify(received_signed_data, hash_true_data))
         print("Message: {} From: {}".format(data, sender.name))
 
 class History:
     def __init__(self, sender, data, data_bytes, receiver):
-        self.sended_time = time.time()
+        local_time = time.localtime(time.time())
+        self.sended_time = time.strftime("%Y-%m-%d %H:%M:%S", local_time)
         self.sender = sender
         self.data = data
         self.data_bytes = data_bytes
         self.receiver = receiver
     def open_data(self): 
         return self.data
+    
+Amy = User('Amy')
+Bob = User('Bob')
+Amy.send_data("Hello", Bob)
+print(Amy.mail_box['Bob'].sended_time)
